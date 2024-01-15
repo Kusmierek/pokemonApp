d81c1931e00673085a83ce5f6c645738bf29566e
Oskar Kusmierek <oskar.kusmierekk@gmail.com>
2024-01-15 23:36:30 +0100

    fix: pokemon form

--- src/app/core/components/pokemon-form/pokemon-form.component.ts
+++ src/app/core/components/pokemon-form/pokemon-form.component.ts
@@ -1,15 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { PokemonFormService } from "../../../shared/service/pokemonForm/pokemon-form.service";
+import { Pokemon } from "src/app/shared/types/pokemon";
 
 @Component({
   selector: "app-pokemon-form",
   templateUrl: "./pokemon-form.component.html",
   styleUrl: "./pokemon-form.component.scss",
 })
-export class PokemonFormComponent {
-  constructor(public pokemonFormService: PokemonFormService) {}
+export class PokemonFormComponent implements OnInit {
+  @Input() public isUpdate!: boolean;
+  @Input() public updatedPokemon!: Pokemon;
+  @Output() public changeVisible: EventEmitter<boolean> = new EventEmitter();
+  public constructor(public pokemonFormService: PokemonFormService) {}
 
-  log() {
-    console.log(this.pokemonFormService.pokemons.controls);
+  public ngOnInit(): void {
+    this.pokemonFormService.addPokemon();
+  }
+
+  public onSubmit(): void {
+    this.pokemonFormService.onSubmit(this.isUpdate, this.updatedPokemon.id);
+    this.changeVisible.emit();
   }
 }
