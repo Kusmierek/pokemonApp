5388b75b9f9b1a0cc7cb7fd1c181adcec11e26e0
Oskar Kusmierek <oskar.kusmierekk@gmail.com>
2024-01-15 23:33:54 +0100

    feat: add errors

---
+++ src/app/pages/error-page/error-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-error-page",
+  templateUrl: "./error-page.component.html",
+  styleUrl: "./error-page.component.scss",
+})
+export class ErrorPageComponent {}
